324b11cf83a01fb7f084f4664b421e4e018acb3a
Cien267 <kiennguyenduc267@gmail.com>
2025-12-02 19:18:53 +0700

    refactor: make menu always appear

--- src/components/layouts/Menu.vue
+++ src/components/layouts/Menu.vue
@@ -8,28 +8,28 @@ const router = useRouter()
 const items = ref([
   {
     label: 'Điểm đến',
-    class: 'pi pi-map-marker',
+    icon: '📍',
     command: () => {
       router.push({ name: ROUTER_NAME_LIST.LIST_DESTINATION_PAGE })
     },
   },
   {
     label: 'Lịch trình',
-    class: 'pi pi-compass',
+    icon: '🗺️',
     command: () => {
       router.push({ name: ROUTER_NAME_LIST.LIST_ITINERARY_PAGE })
     },
   },
   {
-    label: 'Quản lý phương tiện',
-    class: 'pi pi-car',
+    label: 'Phương tiện',
+    icon: '🛵',
     command: () => {
       router.push({ name: ROUTER_NAME_LIST.VEHICLE_PAGE })
     },
   },
   {
-    label: 'Quản lý người dùng',
-    class: 'pi pi-users',
+    label: 'Người dùng',
+    icon: '🫂',
     command: () => {
       router.push({ name: ROUTER_NAME_LIST.USER_PAGE })
     },
@@ -51,12 +51,20 @@ const onDockItemClick = (event: any, item: any) => {
         <a
           v-tooltip.top="item.label"
           href="#"
-          class="shadow-sm p-dock-item-link rounded-lg bg-white dark:!bg-slate-700 flex items-center justify-center !cursor-pointer"
+          class="shadow-lg border border-gray-50 p-dock-item-link rounded-lg bg-white dark:!bg-slate-700 flex items-center justify-center !cursor-pointer"
           @click="onDockItemClick($event, item)"
         >
-          <i :class="item.class" class="!text-2xl"></i>
+          <span class="!text-2xl">{{ item.icon }}</span>
         </a>
       </template>
     </Dock>
   </div>
 </template>
+<style>
+.p-dock-list-container {
+  backdrop-filter: blur(10px);
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  margin-bottom: 20px !important;
+  border: none !important;
+}
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,14 +2,12 @@
 import NavBar from '@/components/layouts/NavBar.vue'
 import Menu from '@/components/layouts/Menu.vue'
 import { useRoute } from 'vue-router'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 const currentRoute = useRoute()
 const classContainer = computed(() => {
   return `plangotrip-${String(currentRoute.name)}`
 })
-
-const showMenu = ref(false)
 </script>
 
 <template>
@@ -17,30 +15,9 @@ const showMenu = ref(false)
     <div class="card plangotrip-layout">
       <nav-bar></nav-bar>
       <slot></slot>
-      <div
-        class="fixed h-2 bottom-0 w-full"
-        @mouseover="showMenu = true"
-        @mouseleave="showMenu = false"
-      >
-        <Transition name="slide-fade">
-          <Menu v-if="showMenu" class="fixed bottom-2 w-full"></Menu>
-        </Transition>
+      <div class="fixed h-2 bottom-0 w-full">
+        <Menu class="fixed bottom-2 w-full"></Menu>
       </div>
     </div>
   </div>
 </template>
-<style>
-.slide-fade-enter-active {
-  transition: all 0.2s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateY(20px);
-  opacity: 0;
-}
-</style>
